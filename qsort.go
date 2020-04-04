1a005f7c6723cd2fce937ba54756e564759c5cec
Smurf <sudaru2@gmail.com>
2020-04-04 17:23:33 +0300

    add float64 qsort;add tests

--- qsort.go
+++ qsort.go
@@ -26,3 +26,30 @@ func QuicksortInt(arr []int) []int {
 
 	return arr
 }
+
+func QuicksortFloat64(arr []float64) []float64 {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	left, right := 0, len(arr)-1
+
+	pivot := len(arr) >> 1
+	//pivot := rand.Int() % len(a)
+
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	for i, _ := range arr {
+		if arr[i] < arr[right] {
+			arr[left], arr[i] = arr[i], arr[left]
+			left++
+		}
+	}
+
+	arr[left], arr[right] = arr[right], arr[left]
+
+	QuicksortFloat64(arr[:left])
+	QuicksortFloat64(arr[left+1:])
+
+	return arr
+}
---
+++ qsort_test.go
@@ -0,0 +1,39 @@
+package gosort
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQuicksortFloat64(t *testing.T){
+	fmt.Println("Start TestQuicksortFloat64")
+	unsorted := []float64{2.0,6.0,1.0,4.0}
+	sorted := []float64{1.0,2.0,4.0,6.0}
+	unsortedAfter := QuicksortFloat64(unsorted)
+	errFlag := true
+	for i:=0 ; i < len(unsorted);i++{
+		if sorted[i] != unsorted[i]{
+			t.Error("Wrong value at pos:",i," expected:",sorted[i]," found:",unsortedAfter[i])
+			errFlag = false
+		}
+	}
+	if errFlag{
+		fmt.Println("TestQuicksortFloat64=[ok]")
+	}
+}
+
+func TestQuicksortInt(t *testing.T){
+	fmt.Println("Start TestQuicksortInt")
+	unsorted := []int{2,6,1,4}
+	sorted := []int{1,2,4,6}
+	unsortedAfter := QuicksortInt(unsorted)
+	errFlag := true
+	for i:=0 ; i < len(unsorted);i++{
+		if sorted[i] != unsortedAfter[i]{
+			t.Error("Wrong value at pos:",i," expected:",sorted[i]," found:",unsortedAfter[i])
+			errFlag = false
+		}
+	}
+	if errFlag{
+		fmt.Println("TestQuicksortInt=[ok]")
+	}
+}
